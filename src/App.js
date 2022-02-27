1cd943f391bcbd9e13897ab9a416ad4bfb50ab59
Akash Lohani <allohani@amazon.com>
2022-02-27 15:00:50 -0800

    Icon update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     // BEM class naming convention
     <div className="app">
-      <h1>Hello Clever Programmer</h1>
       <Header></Header>
       {/*Header*/}
       {/*Sidebar*/}
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Avatar from '@material-ui/core/Avatar';
 
 function Header() {
     return (
         <div className='header'>
-            <h1>I am a header</h1>
+            <MenuIcon />
+            <img
+                className='header_logo' 
+                src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+                alt=''
+            />
+            <input text="text" />
+            <SearchIcon />
+            <VideoCallIcon />
+            <AppsIcon />
+            <NotificationsIcon/>
+            <Avatar />
         </div>
     );
 }
